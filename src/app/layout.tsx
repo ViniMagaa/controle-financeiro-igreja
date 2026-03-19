fb18ec3afea6133fc5222411e505d1dfc37fa649
Vinícius Magalhães <vinisantos2008vs@gmail.com>
2026-03-19 16:59:48 -0300

    feat: initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+
+const ibmPlexSans = IBM_Plex_Sans({
+  variable: "--font-ibm-plex-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Controle Financeiro Igreja",
+  description: "Sistema de controle financeiro para igreja",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="pt-br"
+      className={`${ibmPlexSans.className} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl">Controle Financeiro Igreja</h1>
+    </div>
+  );
+}
